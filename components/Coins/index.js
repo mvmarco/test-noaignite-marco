c4bc38e748458af5fce8b99aaf3b5670014b5995
Marco Corapi <marco.corapi@outlook.it>
2022-04-03 16:50:34 +0200

    basic style on Coins component

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,7 +1,37 @@
-const Coins = ({name}) => {
+import styles from "./Coins.module.css"
+
+const Coins = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+  id,
+}) => {
   return (
-    <div><h1>{name}</h1></div>
-  )
-}
+    <div className={styles.coin_container}>
+      <div className={styles.coin_row}>
+        <img src={image} alt={name} className={styles.coin_img} />
+        <h1 className={styles.coin_h1}>{name}</h1>
+        <p className={styles.coin_symbol}>{symbol}</p>
+      </div>
+      <div className={styles.coin_data}>
+        <p className={styles.coin_price}>${price}</p>
+        <p className={styles.coin_volume}>{volume.toLocaleString()}</p>
+        {/* if the price is negative it will show it in red otherwise in green */}
+        {priceChange < 0 ? (
+          <p className={styles.coin_percent, styles.red}>{priceChange.toFixed(2)}%</p>
+        ) : (
+          <p className={styles.coin_percent, styles.green}>{priceChange.toFixed(2)}%</p>
+        )}
+        <p className={styles.coin_marketcap}>
+          Mkt Cap: ${marketcap.toLocaleString()}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Coins
